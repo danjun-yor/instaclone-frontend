8a22f24f440f0c51db29c7f0ca2cf28188ea04e7
danjun <chljy33@naver.com>
2019-09-11 22:57:48 +0900

    GlobalStyles and Theme setting

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
+import React from "react";
+// import * as Theme from "../Styles/Theme";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
