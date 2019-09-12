79c23a5bf9cef86ba447c767e5c35cf386752f52
danjun <chljy33@naver.com>
2019-09-12 18:52:42 +0900

    basic auth query with graphql

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,14 +3,26 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
+import { useQuery, QueryHookResult } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
+
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
 const App: React.FC = () => {
+  const {
+    data: { isLoggedIn }
+  }: QueryHookResult<{ isLoggedIn: boolean }, any> | any = useQuery(QUERY);
+
   return (
     <>
       <ThemeProvider theme={Theme}>
         <GlobalStyles />
       </ThemeProvider>
-      <AppRouter isLoggedIn={false} />
+      <AppRouter isLoggedIn={isLoggedIn} />
     </>
   );
 };
