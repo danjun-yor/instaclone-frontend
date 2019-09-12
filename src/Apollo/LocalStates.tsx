2b18e5fae56ade52ab07ebfc1fa6c1c2bc4c55d8
danjun <chljy33@naver.com>
2019-09-12 18:26:58 +0900

    apollo client add

---
+++ src/Apollo/LocalStates.tsx
@@ -0,0 +1,26 @@
+export const defaults = {
+  isLoggedIn: localStorage.getItem("token") !== null ? true : false
+};
+
+export const resolvers = {
+  Mutation: {
+    logUserIn: (
+      _: any,
+      { token }: { token: string },
+      { cache }: { cache: any }
+    ) => {
+      localStorage.setItem("token", token);
+      cache.writeData({
+        data: {
+          isLoggedIn: true
+        }
+      });
+      return null;
+    },
+    logUserOut: (_: any, __: any, { cache }: { cache: any }) => {
+      localStorage.removeItem("token");
+      window.location.reload();
+      return null;
+    }
+  }
+};
