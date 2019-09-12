47927c5e9cfa25d73aaeed4f1cf656952abdf293
danjun <chljy33@naver.com>
2019-09-13 00:57:35 +0900

    Auth UI

---
+++ src/Components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.button`
+  width: 100%;
+  border: 0;
+  border-radius: ${props => props.theme.borderRadius};
+  color: white;
+  font-weight: 600;
+  background-color: ${props => props.theme.blueColor};
+  text-align: center;
+  padding: 7px 0px;
+  font-size: 14px;
+`;
+
+const Button = ({ text }: { text: string }) => <Container>{text}</Container>;
+
+export default Button;
